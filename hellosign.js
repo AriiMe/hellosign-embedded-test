f34459c14409a019e054e235b57053984d1c721a
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-07-09 11:31:28 +0200

    working with embedded custom file

--- hellosign.js
+++ hellosign.js
@@ -2,29 +2,52 @@ const hellosign = require("hellosign-sdk")({ key: process.env.HELLOSIGN_API_KEY 
 
 const getEmbedURL = async (github_email, github_username) => {
   let options = {
-    test_mode: 1,
+
+
     clientId: process.env.HELLOSIGN_CLIENT_ID,
-    template_id: process.env.CLA_TEMPLATE_ID,
+    //template_id: process.env.CLA_TEMPLATE_ID,
+
+    title: 'Vertrag GFN',
     subject: 'Student License Agreement',
+    message: 'embedded draft test',
     signers: [
       {
         email_address: github_email,
-        name: 'Student',
+        name: github_username,
         role: process.env.SIGNER_ROLE_NAME
       }
     ],
-    custom_fields: {  //Merge fields defined in the template
-      github_username: github_username,
-      github_email: github_email
-    }
-  }
+
+    // custom_fields: {  //Merge fields defined in the template
+    //   github_username: github_username,
+    //   github_email: github_email,
+    //   // value: "Enter organization name",
+    //   // editor: "signers",
+    //   // required: "true"
+
+    // },
+    files: ['Vertrag_Musterfrau.docx'],
+    use_text_tags: 1,
+    hide_text_tags: 1,
+    test_mode: 1,
+
+
+
+
+  };
+  console.log(options)
 
   // 1st create an embeded signature request using a template
-  let response = await hellosign.signatureRequest.createEmbeddedWithTemplate(options);
+  // let result = hellosign.template.createEmbeddedDraft(options)
+  // let response = await hellosign.signatureRequest.createEmbeddedWithTemplate(result);
+  let response = await hellosign.signatureRequest.createEmbedded(options);
+
   let signature_id = response.signature_request.signatures[0].signature_id;
+
 
   // 2nd fetch the url to embed specific for the first (and only) signer
   let embedded_resp = await hellosign.embedded.getSignUrl(signature_id);
+
   return embedded_resp.embedded.sign_url;
 }
 
